c81ae7f7a58c325da57c0a6945f9dfb66bff0eb5
msesrivera <moises116@live.com.mx>
2024-03-12 00:38:51 -0600

    Update Commit

--- bin/dir_script.dart
+++ bin/dir_script.dart
@@ -22,11 +22,21 @@ Future<void> printDirectoryFiles(Directory dir, int level) async {
 
 Future<void> main() async {
   final Directory current = Directory.current;
-  await Future.forEach(await current.list(recursive: false).toList(),
-      (f) async {
-    print('- ${f.path}');
-    if (f is Directory) {
-      await printDirectoryFiles(f, 1);
-    }
+
+  final List<FileSystemEntity> entities =
+      await current.list(recursive: false).toList();
+
+  final List<Directory> directories = entities.whereType<Directory>().toList();
+
+  await Future.forEach(directories, (entity) async {
+    print('- ${entity.path}');
+    await printDirectoryFiles(entity,1);
+  });
+
+  final List<File> files = entities.whereType<File>().toList();
+
+  files.sort((a, b) => a.path.compareTo(b.path));
+  await Future.forEach(files, (entity) async {
+    print('- ${entity.path}');
   });
 }
